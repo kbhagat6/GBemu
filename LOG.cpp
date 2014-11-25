305d102c6df93241cd06110fd36187035d57c889
kbhagat6 <kbhagat6@gatech.edu>
2014-11-25 18:46:37 -0500

    Create LOG.cpp

---
+++ LOG.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "LOG.h"
+
+
+LOG::LOG(void){
+	_start = 0; 
+}
+
+void LOG::reset(void){
+	time_t *q;
+	time_t stime, p;
+	stime = time(NULL);
+	//p = time(q);
+	char *currenttimestring;
+	char *Buf = new char[2048];
+
+	ctime_s(Buf, 2048,&stime);
+	//stime=sysfm
+	for (int i = 0; i < 24; i++){
+		if (*(Buf+i) != NULL){
+			cout << *(Buf + i);
+		}
+	}
+	//cout << stime << "\n" << p;
+
+	delete[] Buf;
+}
+
+
+void LOG::out(int module, string str){
+
+
+}
+//
+//log = {
+//_start: 0,
+//
+//	reset : function() {
+//	d = new date(); log._start = d.gettime();
+//	},
+//
+//	out : function(module, str) {
+//		t = new date(); ts = t.gettime() - log._start;
+//		console.log('{'+ts+'ms} ['+module+'] '+str);
+//		document.getelementbyid('msg').innerhtml += ('{' + ts + 'ms} [' + module + '] ' + str + '<br>');
+//	}
+//};
